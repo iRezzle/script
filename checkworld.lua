1f41232cabe22ef51a78772a26200e9c759b5a51
iRezzle <116981670+iRezzle@users.noreply.github.com>
2023-04-11 08:07:28 +0700

    Update checkworld.lua

--- checkworld.lua
+++ checkworld.lua
@@ -2,7 +2,7 @@
 
 farmList = {"OMYBQ","UXMXW","EYLBS","UXMXH","KHYGC","UQLNX","OUVSB","JMYRF","FPGVB","ALYNJ","DTIBB","SLBMX","XMAZEF","ZXIHM","ZIIAB","KXOHW","QGYOL","PZEIC","WDUZH","PQEJG","HAHAHZ29","FRSOG","FRNOG","DZUQG","FJYDB","XFUPS","CWQWL","QTFCW","SQJUV","QNMYM","VOWVU","EDIVR","CWVJM","XNEHG","TRKLI","KXOHX","PMLKN","QYEIU","WZEQF","QTFCZ","TOVNV","JXSYJ","QTFCT","DLNKL","SMQQI","HRWDC", "ISXGM","SHSGWY1","CJGCV","KM325","EHBIY","GSZFZ", "NDUGS","KEJWA","NCFZU", "CLVAE","LTFOG","OLTRG","NUQPF","JGBJQ","UMRNT","NIZBT","DAJYD","CXSIV","WTNJR","HKNEX","HTHCO","ZFQTQ","RWJQH","HDPXA", "RXDRB","VTXSH","UXMXZ","TPJUY","QTHBA","TJRZJ","QZWRN","IBBRV","PYRJG","QAVXI","BPLLJ","EFFWN","WSWFG","GOYXM","KLKSZ"}
 tree = 4585 --SEED ID
-webhook = "https://discord.com/api/webhooks/1045224935150329857/MV6_pODRPo6ZwkPUBO5MU1wh9orfl5WGvpddoAYq8azNOAPrL3OtKgjrjpaNg7tJn35a"
+webhookel = "https://discord.com/api/webhooks/1045224935150329857/MV6_pODRPo6ZwkPUBO5MU1wh9orfl5WGvpddoAYq8azNOAPrL3OtKgjrjpaNg7tJn35a"
 
 local function scanTree(itemid)
     local count = 0
@@ -34,7 +34,7 @@ local function scanFossil()
     return count
 end
 
-function webhookel(status)
+function webhookeli(status)
     local wh = {}
     wh.url = webhook
     wh.username = "Rezzle | CHECK FARM"
@@ -47,7 +47,7 @@ local foundNukedFarm = false --[ DON'T CHANGE!!! ]--
 local total = 0
 local hah = 0
 local NUKED = false --[ DON'T CHANGE!!! ]--
-webhookel("@everyone `SCRIPT CHECK FARM MADE By : `<@895235665980194816>")
+webhookeli("@everyone `SCRIPT CHECK FARM MADE By : `<@895235665980194816>")
 for i = 1, #farmList do
     while getBot().world ~= farmList[i]:upper() and not NUKED do
         sendPacket(3, "action|join_request\nname|"..farmList[i].."\ninvitedWorld|0")
@@ -62,16 +62,16 @@ for i = 1, #farmList do
         end
     end
     if getBot().world == farmList[i]:upper() and not NUKED then
-        webhookel("`"..farmList[i]:upper().." | "..scanTree(tree).." READY | "..scanUnready(tree).." UN-READY | "..scanFossil().." FOSSIL`")
+        webhookeli("`"..farmList[i]:upper().." | "..scanTree(tree).." READY | "..scanUnready(tree).." UN-READY | "..scanFossil().." FOSSIL`")
         sleep(1000)
         hah = 0
     elseif getBot().world ~= farmList[i]:upper() and NUKED then
-        webhookel("`"..farmList[i]:upper().." | NUKED`")
+        webhookeli("`"..farmList[i]:upper().." | NUKED`")
         sleep(1000)
         NUKED = false
         hah = 0
         foundNukedFarm = true
     end
 end
-webhookel("`TOTAL NUKED : "..total.." WORLD`")
+webhookeli("`TOTAL NUKED : "..total.." WORLD`")
 removeBot(getBot().name)
